b81abe9c0d857bc2fd14d4d2ee744174ccf18621
hasibulislamha01 <hasibulislamha01@gmail.com>
2024-07-04 17:35:37 +0600

    footer added

--- src/Layout/Root.jsx
+++ src/Layout/Root.jsx
@@ -1,11 +1,15 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../Components/SharedComponents/Navbar/Navbar";
+import Footer from "../Components/Footer/Footer";
 
 const Root = () => {
     return (
         <div className="">
             <Navbar></Navbar>
-            <Outlet></Outlet>
+            <div className="pb-32">
+                <Outlet></Outlet>
+            </div>
+            <Footer></Footer>
         </div>
     );
 };

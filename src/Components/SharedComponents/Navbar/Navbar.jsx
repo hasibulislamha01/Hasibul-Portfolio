dbaee76b37dff64f6c331e106f24a5a0e9c8be14
hasibulislamha01 <hasibulislamha01@gmail.com>
2024-10-06 20:57:51 +0600

    responsive navbar added

--- src/Components/SharedComponents/Navbar/Navbar.jsx
+++ src/Components/SharedComponents/Navbar/Navbar.jsx
@@ -17,7 +17,7 @@ const Navbar = () => {
 
     }, [])
 
-    const routesCollection = [
+    const navigationMenues = [
         {
             title: 'Home',
             link: '/'
@@ -32,6 +32,21 @@ const Navbar = () => {
         },
     ]
 
+    const activeClass = `text-accent` ;
+    const inactiveClass = `text-secondary` ; 
+
+
+    const openSideNav = () => {
+        const sideNav = document.querySelector('.sideNav')
+        sideNav.classList.remove('hidden')
+        sideNav.classList.add('flex')
+    }
+
+    const hideSideBar = () => {
+        const sideNav = document.querySelector('.sideNav')
+        sideNav.classList.remove('flex')
+        sideNav.classList.add('hidden')
+    }
 
 
     return (
@@ -39,9 +54,15 @@ const Navbar = () => {
         <div className={`w-full h-[50px] z-50 fixed bg-primary`}>
             <div className="h-full container mx-auto">
 
-                {/* nav title ----- My name visible in smaller devices*/}
-                <div className="hidden">
-                    <a className="font-semibold text-xl">Hasibul Islam</a>
+                {/* nav title and hamberger icon ----- visible in smaller devices*/}
+                <div className="h-full px-4 md:hidden flex items-center justify-between">
+
+                    <NavLink to={'/'} className='font-bold text-xl text-accent'>Hasibul Islam</NavLink>
+
+                    <div className="" onClick={openSideNav}>
+                        <img src={menuIcon} alt="menu icon" className="h-full" />
+                    </div>
+
                 </div>
 
                 {/* horizontal navbar --- (visible in larger screens) */}
@@ -55,7 +76,7 @@ const Navbar = () => {
                     </NavLink>
 
                     {
-                        routesCollection.map(route =>
+                        navigationMenues.map(route =>
                             <NavLink
                                 key={route.link}
                                 to={route.link}
@@ -66,8 +87,26 @@ const Navbar = () => {
                         )
                     }
 
-                    <img src={menuIcon} alt="" />
                 </div>
+
+                {/* vertical navbar */}
+                <div className="sideNav hidden shadow-lg shadow-gray-400 w-[250px] h-screen fixed top-0 right-0  bg-primary bg-opacity-95 backdrop-blur-lg  flex-col">
+
+                    {/* close icon */}
+                    <svg onClick={hideSideBar} className="m-2 p-2 cursor-pointer rounded-full hover:bg-slate-800" xmlns="http://www.w3.org/2000/svg" height="40px" viewBox="0 -960 960 960" width="40px" fill="#e8eaed"><path d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z" /></svg>
+                    <div className="w-full pt-4 flex flex-col justify-start items-start">
+                        {
+                            navigationMenues?.map(item =>
+                                <NavLink
+                                    key={item.link}
+                                    to={item.link}
+                                    className={({ isActive }) => isActive ? `${activeClass}  w-full  px-8 py-3 hover:bg-slate-800` : `${inactiveClass} w-full py-3 px-8 hover:bg-slate-800`}
+                                >{item.title}</NavLink>
+                            )
+                        }
+                    </div>
+                </div>
+
             </div>
         </div>
     );
